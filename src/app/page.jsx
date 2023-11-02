fbe57eaebe57e2e005f094843fc97e3de3d067bb
asep3003 <saepulloha133@gmail.com>
2023-11-02 09:51:12 +0700

    SESI7 - Random Data Dengan Logika

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,12 @@
 import AnimeList from "@/components/AnimeList";
 import Header from "@/components/AnimeList/Header";
-import { getAnimeResponse, getNestedAnimeResponse } from "@/libs/api-libs";
+import { getAnimeResponse, getNestedAnimeResponse, reproduce } from "@/libs/api-libs";
 
 const Page = async () => {
 
   const topAnime = await getAnimeResponse("top/anime", "limit=8");
   let recommendedAnime = await getNestedAnimeResponse("recommendations/anime", "entry");
-  recommendedAnime = { data: recommendedAnime.slice(0, 4) };
+  recommendedAnime = reproduce(recommendedAnime, 2);
 
   return (
     <>
