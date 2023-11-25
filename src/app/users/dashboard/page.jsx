07ab1a5d2ee39eeee91a4fc8ee877fce6e81d9c1
Mohamad Asep Saepulloh <saepulloha133@gmail.com>
2023-11-26 03:28:35 +0700

    SESI9 - Validasi User Dashboard

--- src/app/users/dashboard/page.jsx
+++ src/app/users/dashboard/page.jsx
@@ -1,14 +1,18 @@
 import { authUserSession } from '@/libs/auth-libs';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Page = async() => {
   const user = await authUserSession();
 
   return (
-    <div className="text-color-primary">
-      <h3>Dashboard</h3>
-      <h5>Welcome, {user.name}</h5>
-      <Image src={user.image} alt="..." width={250} height={250} />
+    <div className="mt-8 text-color-primary flex flex-col justify-center items-center">
+      <h5 className="text-2xl font-bold">Welcome, {user?.name}</h5>
+      <Image src={user?.image} alt="..." width={250} height={250} />
+      <div className="flex flex-wrap gap-4 py-8">
+        <Link className="bg-color-accent text-color-dark font-bold py-3 px-4 text-xl" href="/users/dashboard/collection">My Collection</Link>
+        <Link className="bg-color-accent text-color-dark font-bold py-3 px-4 text-xl" href="/users/dashboard/comment">My Comment</Link>
+      </div>
     </div>
   )
 }
