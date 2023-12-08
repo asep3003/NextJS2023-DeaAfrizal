2044cfcea53f8362a0dddeecabdfcef78d335afd
Mohamad Asep Saepulloh <saepulloha133@gmail.com>
2023-12-08 09:26:09 +0700

    SESI11 - Fitur KOMENTAR Anime

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,4 +1,6 @@
 import CollectionButton from "@/components/AnimeList/CollectionButton";
+import CommentBox from "@/components/AnimeList/CommentBox";
+import CommentInput from "@/components/AnimeList/CommentInput";
 import VideoPlayer from "@/components/Utilities/VideoPlayer";
 import { getAnimeResponse } from "@/libs/api-libs";
 import { authUserSession } from "@/libs/auth-libs";
@@ -20,7 +22,7 @@ const Page = async ({ params: {id} }) => {
       <div className="pt-4 px-4">
         <h3 className="text-2xl text-color-primary">{anime.data.title} - {anime.data.year}</h3>
         {
-          !collection && user && <CollectionButton anime_mal_id={id} user_email={user?.email} />
+          !collection && user && <CollectionButton anime_mal_id={id} user_email={user?.email} anime_image={anime.data.images.webp.image_url} anime_title={anime.data.title} />
         }
       </div>
       <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-x-auto">
@@ -44,6 +46,11 @@ const Page = async ({ params: {id} }) => {
       <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-primary">
         <Image src={anime.data.images.webp.image_url} alt={anime.data.images.webp.image_url} width={250} height={250} className="w-full rounded object-cover" />
         <p className="text-justify text-xl">{anime.data.synopsis}</p>
+      </div>
+      <div className="p-4">
+        <h3 className="text-color-primary text-2xl mb-2">Komentar Penonton</h3>
+        <CommentBox anime_mal_id={id} />
+        {user && <CommentInput anime_mal_id={id} user_email={user?.email} username={user?.name} anime_title={anime.data.title} />}
       </div>
       <div>
         <VideoPlayer youtubeId={anime.data.trailer.youtube_id} />
--- src/app/users/dashboard/collection/page.jsx
+++ src/app/users/dashboard/collection/page.jsx
@@ -2,6 +2,7 @@ import Header from "@/components/Dashboard/Header"
 import { authUserSession } from "@/libs/auth-libs"
 import Image from "next/image"
 import Link from "next/link"
+import prisma from "@/libs/prisma"
 
 const Page = async () => {
   const user = await authUserSession()
@@ -17,10 +18,10 @@ const Page = async () => {
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
         {collection.map((collect, index) => {
           return (
-            <Link key={index} href={`/anime/${collect.anime_mal_id}`} className="relative border-2 border-color-accent">
-              <Image src="" alt="" width={350} height={350} className="w-full" />
+            <Link key={index} href={`/anime/${collect.anime_mal_id}`} className="relative">
+              <Image src={collect.anime_image} alt={collect.anime_image} width={350} height={350} className="w-full" />
               <div className="absolute flex items-center justify-center bottom-0 w-full bg-color-accent h-16">
-                <h5 className="text-xl text-center">{collect.anime_mal_id}</h5>
+                <h5 className="text-xl text-center">{collect.anime_title}</h5>
               </div>
             </Link>
           )
